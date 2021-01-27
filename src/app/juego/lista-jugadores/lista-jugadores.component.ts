adf08e62f0f7a694c41af8e0c8329b7bf861d179
SoftHyde <luucas125@outlook.com.ar>
2021-01-27 18:22:21 -0300

    agregado los botones que se adecuan a la cant de jugadores

--- src/app/juego/lista-jugadores/lista-jugadores.component.ts
+++ src/app/juego/lista-jugadores/lista-jugadores.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Jugador } from 'src/app/model/jugador';
+import { JugadoresService } from 'src/app/services/jugadores.service';
 
 @Component({
   selector: 'app-lista-jugadores',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaJugadoresComponent implements OnInit {
 
-  constructor() { }
+  constructor(private jugadoresService: JugadoresService) { }
 
   ngOnInit(): void {
+  }
+
+  obtenerJugadores(): Array<Jugador>{
+    return this.jugadoresService.players;
   }
 
 }
